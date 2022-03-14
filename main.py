2d158cb9d02ddf9370b14483d0b40ca73902d839
Alejandro Viveros <alejandroviverosm@gmail.com>
2022-03-14 16:26:44 -0300

    Se realizan ejemplos de concatenaciones

---
+++ main.py
@@ -0,0 +1,15 @@
+# cadena
+miGrupoFavorito = "Aerosmith"
+comentario = "the best rock band"
+# print("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)
+print("Mi grupo favorito es:", miGrupoFavorito, comentario)
+
+numero1 = "1"
+numero2 = "2"
+print("Concatenacion:", numero1 + numero2)
+print("Suma:", int(numero1) + int(numero2))
+
+# numero1 = 1
+# numero2 = 2
+
+# print("Suma:", numero1 + numero2)
