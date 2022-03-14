b605a976168b258544037cd098cfb1e34e5c8aac
Alejandro Viveros <alejandroviverosm@gmail.com>
2022-03-14 16:59:05 -0300

    se realizan ejemplos con valores booleanos

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-# cadena
-miGrupoFavorito = "Aerosmith"
-comentario = "the best rock band"
-# print("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)
-print("Mi grupo favorito es:", miGrupoFavorito, comentario)
+#tipos booleanos
+miVariable = False
+print(miVariable)
 
-numero1 = "1"
-numero2 = "2"
-print("Concatenacion:", numero1 + numero2)
-print("Suma:", int(numero1) + int(numero2))
+miVariable = 1 > 2
+print(miVariable)
 
-# numero1 = 1
-# numero2 = 2
-
-# print("Suma:", numero1 + numero2)
+if miVariable:
+    print("El resultado fue verdadero")
+else:
+    print("El resultado es falso")
