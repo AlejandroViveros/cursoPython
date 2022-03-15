324b74ef7feda3544fcc535ef99b1bc750e2667c
Alejandro Viveros <alejandroviverosm@gmail.com>
2022-03-15 17:44:39 -0300

    ejercicio basico de funcion input, convirtiendo este input en numero entero

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-# funcion input para procesar entrada de usuario
-numero1 = int(input("escribe el primer numero: "))
-numero2 = int(input("escribe el segundo numero: "))
-resultado = numero1 + numero2
-print("la suma de los numeros es: " ,resultado)
-
-
+calificacionDia = int(input("Como estuvo tu dia del 1 al 10 ?: "))
+print("Mi dia estuvo de: ", calificacionDia)
